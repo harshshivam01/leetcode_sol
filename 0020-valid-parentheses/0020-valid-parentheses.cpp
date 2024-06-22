9a0dd76a499bc136aff4abd81a26e45cc9222a2d
harsh shivam <113056031+harshshivam02@users.noreply.github.com>
2024-06-22 10:18:45 +0530

    Time: 0 ms (100%), Space: 7.5 MB (60.88%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        // for (int i = 0; i < s.size(); i++) {
-        //     if (st.empty() && s[i] == ')' || s[i] == ']' || s[i] == '}') {
-        //         return false;
-        //     } else if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
-        //         st.push(s[i]);
-        //     } else {
-        //          if(s[i]==')'&&st.top()!='(')return false;
-        //          else if(s[i]==']'&&st.top()!='[')return false;
-        //          else  if(s[i]=='}'&&st.top()!='{')return false;
-        //      st.pop();
-        //     }
-        // }
-
-        // return  st.empty();
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] == ')' && st.size() > 0 && st.top() == '(') {
-                st.pop();
-            } else if (s[i] == ']' && st.size() > 0 && st.top() == '[') {
-                st.pop();
-            } else if (s[i] == '}' && st.size() > 0 && st.top() == '{') {
-                st.pop();
+            if (st.empty() && (s[i] == ')' || s[i] == ']' || s[i] == '}')) {
+                return false;
+            } else if (!st.empty() && s[i] == ')' || s[i] == ']' ||
+                       s[i] == '}') {
+                if ((s[i] == ')' && st.top() == '(') ||
+                     (s[i] == ']' && st.top() == '[') ||
+                    (s[i] == '}' && st.top() == '{')) {
+                    st.pop();
+                } else
+                    return false;
             } else {
                 st.push(s[i]);
             }
         }
+
         return st.empty();
+        // for (int i = 0; i < s.size(); i++) {
+        //     if (s[i] == ')' && st.size() > 0 && st.top() == '(') {
+        //         st.pop();
+        //     } else if (s[i] == ']' && st.size() > 0 && st.top() == '[') {
+        //         st.pop();
+        //     } else if (s[i] == '}' && st.size() > 0 && st.top() == '{') {
+        //         st.pop();
+        //     } else {
+        //         st.push(s[i]);
+        //     }
+        // }
+        // return st.empty();
     }
 };
