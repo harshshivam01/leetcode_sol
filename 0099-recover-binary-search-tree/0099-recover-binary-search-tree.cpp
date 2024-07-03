4e6f1ef6b373a945f71f19ed15f703b9199ce145
harsh shivam <113056031+harshshivam02@users.noreply.github.com>
2024-07-04 01:07:46 +0530

    Time: 16 ms (81.67%), Space: 60.4 MB (42.64%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -11,58 +11,30 @@
  * };
  */
 class Solution {
+private:
+    TreeNode* prev;
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+private:
+   void inorder(TreeNode*root){
+    if(root==NULL)return ;
+    inorder(root->left);
+    if(prev!=NULL &&(root->val<prev->val))
+       if(first==NULL){
+        first=prev;
+        middle=root; 
+       }
+       else last=root;
+  prev=root;
+  inorder(root->right);
+   }
 public:
-    void inorder(TreeNode* root, vector<int>& ans) {
-        if (root == NULL)
-            return;
-        inorder(root->left, ans);
-        ans.push_back(root->val);
-        inorder(root->right, ans);
-    }
     void recoverTree(TreeNode* root) {
-        vector<int> sortedVals;
-        inorder(root, sortedVals);
-        sort(sortedVals.begin(), sortedVals.end());
-        TreeNode* current = root;
-        int index = 0;
-
-        while (current != nullptr) {
-
-            if (current->left == nullptr) {
-
-                if (current->val != sortedVals[index]) {
-
-                    current->val = sortedVals[index];
-                }
-                ++index;
-
-                current = current->right;
-            } else {
-
-                TreeNode* predecessor = current->left;
-                while (predecessor->right != nullptr &&
-                       predecessor->right != current) {
-                    predecessor = predecessor->right;
-                }
-
-                if (predecessor->right == nullptr) {
-
-                    predecessor->right = current;
-
-                    current = current->left;
-                } else {
-
-                    predecessor->right = nullptr;
-
-                    if (current->val != sortedVals[index]) {
-
-                        current->val = sortedVals[index];
-                    }
-                    ++index;
-
-                    current = current->right;
-                }
-            }
-        }
+        first=middle=last=NULL;
+        prev=new TreeNode(INT_MIN);
+        inorder(root);
+        if(first&&last)swap(first->val,last->val);
+        else if(first&&middle)swap(first->val,middle->val);
     }
 };
