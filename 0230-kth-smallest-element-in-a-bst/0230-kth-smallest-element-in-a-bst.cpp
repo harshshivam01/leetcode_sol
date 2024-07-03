718e7ccabfe3c773782f56a7109a982659b57dfa
harsh shivam <113056031+harshshivam02@users.noreply.github.com>
2024-07-03 16:25:20 +0530

    Time: 16 ms (32.83%), Space: 22.5 MB (59.87%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -1,38 +1,19 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-public:
-     int helper(TreeNode* root, unordered_map<TreeNode*,int>&mp){
-        if(root==nullptr)return 0;
-        int left=helper(root->left,mp);
-        int right=helper(root->right,mp);
-        mp[root]=left;
-        return left+right+1;
+ public:
+  int kthSmallest(TreeNode* root, int k) {
+    const int leftCount = countNodes(root->left);
 
+    if (leftCount == k - 1)
+      return root->val;
+    if (leftCount >= k)
+      return kthSmallest(root->left, k);
+    return kthSmallest(root->right, k - 1 - leftCount);  
+  }
 
-     }
-    int kthSmallest(TreeNode* root, int k) {
-        unordered_map<TreeNode*,int>mp;
-        helper(root,mp);
-         while (root != nullptr) {
-        int leftSize = mp[root] + 1;
-        if (leftSize == k) return root->val;
-        else if (leftSize > k) root = root->left;
-        else {
-            root = root->right;
-            k -= leftSize;
-        }
-    }
-
-    return -1;
-    }
+ private:
+  int countNodes(TreeNode* root) {
+    if (root == nullptr)
+      return 0;
+    return 1 + countNodes(root->left) + countNodes(root->right);
+  }
 };
