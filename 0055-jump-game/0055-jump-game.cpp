19affeb1dcc28dbe55ba5e3c09de97251a848218
harsh shivam <113056031+harshshivam02@users.noreply.github.com>
2024-07-23 23:55:38 +0530

    Time: 43 ms (65.61%), Space: 51.1 MB (23.74%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    int dp[100005];
-    bool f(vector<int>& nums, int n) {
-      
-        if(n>=nums.size()-1)return true;
-        if (dp[n] != -1)
-            return dp[n];
-         int i=1;
-        while ( i<= nums[n]) {
-            if(f(nums, i+ n)){
-                return dp[n]=true ;
-            }
-            i++;
-        }
-
-        return  dp[n]=false;
-    }
     bool canJump(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return f(nums, 0);
+        int jump = 0;
+        for(int i=0;i<nums.size();i++){
+            if(i>jump){
+                return false;
+            }
+            jump = max(jump, i+nums[i]);
+        }
+        return true;
     }
 };
