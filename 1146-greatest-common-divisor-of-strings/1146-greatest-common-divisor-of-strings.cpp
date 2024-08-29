091357e0873d473d629738cb1f114b6b68e3e35d
harsh shivam <113056031+harshshivam02@users.noreply.github.com>
2024-08-29 18:58:23 +0530

    Time: 6 ms (21.68%), Space: 8.6 MB (62.76%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    bool check(string ans, string str1) {
-        int n = ans.size();
-        int m = str1.size();
-        if (m % n != 0)
-            return false;
-        for (int i = 0; i < m; i += n) {
-             if (str1.substr(i, n) != ans)return  false ;
-        }
-        return true;
-    }
     string gcdOfStrings(string str1, string str2) {
-        int m = str1.size();
-        int n = str2.size();
-      
-        if (n > m)
-            return gcdOfStrings(str2,str1);
-        int i = 0;
-        string ans = "";
-        while (i < n) {
-            ans = str2.substr(0,n-i);
-            if (check(ans, str1)&&check(ans,str2)) {
-                return ans;
-            } else {
-                i++;
-            }
+        if (str1 + str2 != str2 + str1) {
+            return "";
         }
-        return "";
+        return str1.substr(0, gcd(str1.length(), str2.length()));
+    }
+    
+    
+    int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
     }
 };
