967bb7023f8ee2170a7f7b78ea596ae0a8b70795
harsh shivam <113056031+harshshivam02@users.noreply.github.com>
2024-09-18 22:40:28 +0530

    Time: 15 ms (70.72%), Space: 17.9 MB (39.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,37 +1,57 @@
 class Solution {
 public:
-    bool dfs(int i, vector<int>& visi, vector<int> &pathvisi,
+    bool dfs(int i, vector<int>& visi, vector<int>& pathvisi,
              vector<int> adjList[]) {
         pathvisi[i] = 1;
         visi[i] = 1;
-        for ( const auto& it : adjList[i]) {
+        for (const auto& it : adjList[i]) {
             if (visi[it] == 0) {
-                if (dfs(it,visi,pathvisi,adjList))return true;
-            }
-            else if(pathvisi[it]==1)return true;
+                if (dfs(it, visi, pathvisi, adjList))
+                    return true;
+            } else if (pathvisi[it] == 1)
+                return true;
         }
-        pathvisi[i]=0;
-        return false ;
+        pathvisi[i] = 0;
+        return false;
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 
         // int n = prerequisites.size();
         // int m = prerequisites[0].size();
+        int V = numCourses;
+        vector<int> adj[numCourses];
 
-        vector<int> adjList[numCourses];
-
-         for (const auto& pre : prerequisites) {
-            adjList[pre[1]].push_back(pre[0]);
+        for (const auto& pre : prerequisites) {
+            adj[pre[1]].push_back(pre[0]);
+        }
+        int count = 0;
+        queue<int> q;
+        vector<int> indegree(V + 1, 0);
+        for (int i = 0; i < V; i++) {
+            for (auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
         }
 
-        vector<int> visi( numCourses, 0);
-        vector<int> pathvisi( numCourses, 0);
-        for (int i = 0; i < numCourses; i++) {
-            if (visi[i] == 0)
-               if(dfs(i, visi, pathvisi, adjList))return false;
-               
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            count++;
+            for (auto it : adj[x]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
         }
-      return true;
-
+        if (count == V)
+            return true;
+        else
+            return false;
     }
 };
