77ca56112304aa26da6b74d8c9071b4f872f207e
harsh shivam <113056031+harshshivam02@users.noreply.github.com>
2024-09-19 17:02:01 +0530

    Time: 141 ms (49.13%), Space: 64.6 MB (40.4%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -34,7 +34,7 @@ public:
             }
         }
         for(int i=0;i<n;i++){
-           if(check[i]==1){
+           if(pathvisi[i]==0){
             ans.push_back(i);
            }
         }
