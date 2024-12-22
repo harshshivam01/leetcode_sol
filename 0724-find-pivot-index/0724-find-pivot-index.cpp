d6b5b018fb44ceb82fbbe60b747c044a9ed20ad1
harsh shivam <113056031+harshshivam01@users.noreply.github.com>
2024-12-22 21:23:09 +0530

    Time: 3 ms (25.65%), Space: 37.1 MB (6.63%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,30 +1,25 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        vector<int> leftsum(nums.size(), 0);
-        vector<int> rightsum(nums.size(), 0);
-        
-        // Build the left sum array
+        vector<int> leftsum(nums.size(),0);
+        vector<int> rightsum(nums.size(),0);
         int left = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            left += nums[i];
-            leftsum[i] = left;
-        }
-        
-        // Build the right sum array
         int right = 0;
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            right += nums[i];
-            rightsum[i] = right;
-        }
-        
-        // Compare leftsum and rightsum to find the pivot index
         for (int i = 0; i < nums.size(); i++) {
-            if (leftsum[i] == rightsum[i]) {
-                return i;
-            }
+            left+=nums[i];
+            leftsum[i]=left;
         }
+         for (int i = nums.size()-1; i >=0; i--) {
+            right+=nums[i];
+            rightsum[i]=right;
+        }
+        left=0;
         
-        return -1; // Return -1 if no pivot index is found
+     for(int i=0;i<leftsum.size();i++){
+        if(leftsum[i]==rightsum[i]){
+            return i;
+        }
+     }
+        return -1;
     }
 };
